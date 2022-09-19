f03ef07020bcd797d5ac874cec9c52b5113ec67c
LanHuong142002 <lanhuong142002@gmail.com>
2022-09-19 17:26:18 +0700

    #120 Refactor: add examples of DOM(attribute, class, style) and findingElement

---
+++ javascript/DOMexamples/example4/findingElement.js
@@ -0,0 +1,10 @@
+const demoId = document.getElementById("demoId");
+console.log(demoId);
+
+const demoClass = document.getElementsByName("demoClass");
+const demoClasses = document.getElementsByClassName("demoClass");
+console.log(demoClass);
+console.log(demoClasses);
+
+const demoTag = document.getElementsByTagName("p");
+console.log(demoTag);
