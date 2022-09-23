0b2e1f1b140afb191c571284d525aff456a3380e
Huong <lanhuong142002@gmail.com>
2022-09-23 14:11:19 +0700

    #122 Refactor: add file html and change class name

--- javascript/exercise1/ex1/index.js
+++ javascript/exercise1/ex1/index.js
@@ -1,6 +1,6 @@
-const logo = document.querySelector('.lnXdpd');
+const logo = document.querySelector('.logo');
 const url = 'https://logos-world.net/wp-content/uploads/2020/10/Yahoo-Logo-700x394.png'
 logo.setAttribute('srcset', url);
 
-const button = document.querySelector('.CcAdNb');
+const button = document.querySelector('.search');
 button.innerHTML = 'Yahoo !';
