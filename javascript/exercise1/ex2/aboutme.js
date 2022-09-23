0c970d466f1509f2972991f69a59ccaa6daa1de3
Huong <lanhuong142002@gmail.com>
2022-09-23 15:29:22 +0700

    #122 Refactor: add liLength to get length

--- javascript/exercise1/ex2/aboutme.js
+++ javascript/exercise1/ex2/aboutme.js
@@ -2,9 +2,10 @@ const liText = document.querySelectorAll('li');
 const nickname = document.getElementById('nickname');
 const favorites = document.getElementById('favorites');
 const hometown = document.getElementById('hometown');
-
+const liLength = liText.length;
 // Change the body style so it has a font-family of "Arial, sans-serif".
-for (let i = 0; i < liText.length; i += 1) {
+
+for (let i = 0; i < liLength; i += 1) {
   liText[i].style.fontFamily = 'Arial, sans-serif';
   liText[i].classList.add('listitem');
 }
@@ -15,10 +16,10 @@ favorites.innerHTML = 'Sleep';
 hometown.innerHTML = 'DaNang';
 
 const liTextRed = document.querySelectorAll('.listitem');
-for (let i = 0; i < liText.length; i += 1) {
+for (let i = 0; i < liLength; i += 1) {
   liTextRed[i].style.color = 'red';
 }
 
 const img = document.createElement('img');
 document.body.appendChild(img);
-img.setAttribute('src', 'https://logos-world.net/wp-content/uploads/2020/10/Yahoo-Logo-700x394.png')
+img.setAttribute('src', 'https://logos-world.net/wp-content/uploads/2020/10/Yahoo-Logo-700x394.png');
