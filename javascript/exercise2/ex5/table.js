b7f4c97f1d6dc924bce067421306bfa45c538048
LanHuong142002 <lanhuong142002@gmail.com>
2022-09-26 14:58:13 +0700

    #122 Refactor: change from EventListener to inline event

--- javascript/exercise2/ex5/table.js
+++ javascript/exercise2/ex5/table.js
@@ -1,11 +1,7 @@
-const button = document.querySelector('input');
-
-const insertRows = () => {
+function insertRows() {
   const list = document.querySelector('#sampleTable').insertRow(0);
   const list1 = list.insertCell(0);
   const list2 = list.insertCell(0);
   list1.innerHTML = 'New Cell';
   list2.innerHTML = 'New Cell';
-};
-
-button.addEventListener('click', insertRows);
+}
--- javascript/exercise2/ex6/index.js
+++ javascript/exercise2/ex6/index.js
@@ -1,12 +1,8 @@
-const button = document.querySelector('.button');
-
-const changeContent = () => {
+function changeContent() {
   const rowValue = +document.querySelector('.row').value;
   const columnValue = +document.querySelector('.column').value;
   const content = document.querySelector('.content').value;
   const list = document.querySelector('#myTable').rows[rowValue - 1];
 
   list.querySelectorAll('td')[columnValue - 1].innerHTML = content;
-};
-
-button.addEventListener('click', changeContent);
+}
