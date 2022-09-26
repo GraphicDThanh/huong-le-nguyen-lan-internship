c6ee7547ba6482b1780490f1cafc4ae1fdea4669
LanHuong142002 <lanhuong142002@gmail.com>
2022-09-26 15:23:41 +0700

    #122 Refactor: add comment for prevent default event

--- javascript/exercise2/ex2/index.js
+++ javascript/exercise2/ex2/index.js
@@ -3,6 +3,7 @@ const firstName = document.querySelector('.firstName');
 const lastName = document.querySelector('.lastName');
 
 const getFormValue = (e) => {
+  // This event is used to avoid the page reload of the submit event
   e.preventDefault();
   console.log(firstName.value);
   console.log(lastName.value);
