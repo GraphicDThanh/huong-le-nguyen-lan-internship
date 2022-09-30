f5dca5b29a2cbc4640b82a52cb6469b7d086572f
LanHuong142002 <lanhuong142002@gmail.com>
2022-09-30 16:30:12 +0700

    #122 Refactor: remove redundant code in css, add comment and fixbug in delete, add function

--- javascript/exercise_table/table.js
+++ javascript/exercise_table/table.js
@@ -3,18 +3,21 @@ const tableBody = document.querySelector('tbody');
 
 const tasks = [
   {
+    id: 0,
     name: 'Write pomodoro app',
     pomodoroDone: 1,
     pomodoroCount: 4,
     isFinished: false,
   },
   {
+    id: 1,
     name: 'Write pomodoro app',
     pomodoroDone: 1,
     pomodoroCount: 4,
     isFinished: false,
   },
   {
+    id: 2,
     name: 'Write pomodoro',
     pomodoroDone: 1,
     pomodoroCount: 4,
@@ -22,18 +25,21 @@ const tasks = [
   },
 ];
 
+// func create tr element
 function createElement(taskName, pomodoroDone, pomodoroCount, isFinished, id) {
   const trElement = document.createElement('tr');
   trElement.classList.add('task');
   trElement.setAttribute('id', id);
 
   trElement.innerHTML = `
-  <td class="task-details">${taskName}</td>
+  <td class="task-details">
+    <span class="task-name">${taskName}</span>
+  </td>
   <td class="task-details">
     <span class="pomodoro-done">${pomodoroDone}</span> /
     <span class="pomodoro-count">${pomodoroCount}</span> pomodoro
   </td>
-  <td class="task-details">${isFinished ? `Finished <button type="button" class="btn btn-delete" data-id="${id}">Delete task</button>`
+  <td class="task-details">${isFinished ? `<span>Finished</span> <button type="button" class="btn btn-delete" data-id="${id}">Delete task</button>`
     : `<button type="button" class="btn btn-done" data-id="${id}">Done</button>
       <button type="button" class="btn btn-increase" data-id="${id}">Increase Pomodoro Count</button>
       <button type="button" class="btn btn-delete" data-id="${id}">Delete task</button>`}</td>
@@ -42,75 +48,91 @@ function createElement(taskName, pomodoroDone, pomodoroCount, isFinished, id) {
   return trElement;
 }
 
+// func find task from id
 function findTask(indexTask) {
   const idTask = document.getElementById(indexTask);
   return idTask;
 }
 
+// delete task
 const deleteTask = (e) => {
-  const indexTask = e.target.dataset.id;
-  const taskRemove = findTask(indexTask);
+  const indexTask = Number(e.target.dataset.id);
+  const taskElement = findTask(indexTask);
+  const indexTaskInArray = tasks.findIndex((task) => task.id === indexTask);
 
-  tasks.splice(indexTask, 1);
-  taskRemove.remove();
+  tasks.splice(indexTaskInArray, 1);
+  taskElement.remove();
 };
 
+// increase pomodoro
 const increaseTask = (e) => {
   const indexTask = e.target.dataset.id;
   const task = findTask(indexTask);
   let pomodoroDoneValue = +task.childNodes[3].querySelector('.pomodoro-done').innerText;
-  const pomodoroCountValue = task.childNodes[3].querySelector('.pomodoro-count').innerText;
-  console.log(pomodoroCountValue);
+  const pomodoroCountValue = +task.childNodes[3].querySelector('.pomodoro-count').innerText;
+
   if (pomodoroDoneValue < pomodoroCountValue) {
+    tasks[indexTask].pomodoroDone += 1;
     task.childNodes[3].querySelector('.pomodoro-done').innerHTML = `${pomodoroDoneValue += 1}`;
   }
 };
 
+// done pomodoro
 const doneTask = (e) => {
-  const btnDone = e.target.parentElement.querySelector('.btn-done');
-  const btnIncrease = e.target.parentElement.querySelector('.btn-increase');
+  const targetElement = e.target;
+
+  const indexTask = targetElement.dataset.id;
+  const btnDone = targetElement.parentElement.querySelector('.btn-done');
+  const btnIncrease = targetElement.parentElement.querySelector('.btn-increase');
+  const btnDelete = targetElement.parentElement.querySelector('.btn-delete');
   const spanElement = document.createElement('span');
 
-  spanElement.innerHTML = 'Finished';
-  e.target.parentNode.appendChild(spanElement);
+  tasks[indexTask].isFinished = true;
+  spanElement.innerHTML = 'Finished ';
+  targetElement.parentNode.insertBefore(spanElement, btnDelete);
 
   btnDone.remove();
   btnIncrease.remove();
 };
 
+// event of three buttons
 const handleEvents = () => {
-  Array.from(document.getElementsByClassName('btn-delete')).forEach((btnDelete) => {
-    btnDelete.addEventListener('click', deleteTask);
+  const buttonsDelete = document.getElementsByClassName('btn-delete');
+  const buttonsDone = document.getElementsByClassName('btn-done');
+  const buttonsIncrease = document.getElementsByClassName('btn-increase');
+
+  Array.from(buttonsDelete).forEach((btn) => {
+    btn.addEventListener('click', deleteTask);
   });
 
-  Array.from(document.getElementsByClassName('btn-done')).forEach((btnDone) => {
-    btnDone.addEventListener('click', doneTask);
+  Array.from(buttonsDone).forEach((btn) => {
+    btn.addEventListener('click', doneTask);
   });
 
-  Array.from(document.getElementsByClassName('btn-increase')).forEach((btnIncrease) => {
-    btnIncrease.addEventListener('click', increaseTask);
+  Array.from(buttonsIncrease).forEach((btn) => {
+    btn.addEventListener('click', increaseTask);
   });
 };
 
+// render table
 const renderTasks = () => {
-  // eslint-disable-next-line array-callback-return
-  tasks.map((task, id) => {
+  tasks.forEach((task) => {
     tableBody.append(
-      createElement(task.name, task.pomodoroDone, task.pomodoroCount, task.isFinished, id),
+      createElement(task.name, task.pomodoroDone, task.pomodoroCount, task.isFinished, task.id),
     );
-  }).join('');
+  });
 
   handleEvents();
 };
 
+// add tasks
 const addTask = (e) => {
   // This event is used to avoid the page reload of the submit event
   e.preventDefault();
   const inputTask = document.querySelector('.input-task-name');
   const errorMessage = document.querySelector('.error');
-  const selectPomodoro = document.querySelector('.select-pomodoro').selectedIndex;
+  const selectPomodoro = document.querySelector('.select-pomodoro').value;
   const taskName = inputTask.value;
-  const count = selectPomodoro + 1;
 
   if (taskName === '') {
     errorMessage.style.display = 'block';
@@ -118,9 +140,10 @@ const addTask = (e) => {
     errorMessage.innerText = 'Do not empty';
   } else {
     tasks.push({
+      id: tasks.length > 0 ? tasks.length : 0,
       name: taskName,
       pomodoroDone: 0,
-      pomodoroCount: count,
+      pomodoroCount: selectPomodoro,
       isFinished: false,
     });
 
@@ -128,10 +151,12 @@ const addTask = (e) => {
     inputTask.classList.remove('valid');
 
     tableBody.appendChild(
-      createElement(taskName, 0, count, false, tasks.length),
+      createElement(taskName, 0, selectPomodoro, false, tasks.length - 1),
     );
+
     handleEvents();
   }
+
   taskForm.reset();
 };
 
