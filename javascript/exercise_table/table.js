1aada6c2004923d578d657748142a9d3d16434c8
LanHuong142002 <lanhuong142002@gmail.com>
2022-10-03 16:15:54 +0700

    #122 Refactor: use more object, change message error, create common method, comment more details, catch variable

--- javascript/exercise_table/table.js
+++ javascript/exercise_table/table.js
@@ -1,54 +1,40 @@
+/* eslint-disable import/extensions */
+import tasks from './data.js';
+
 const taskForm = document.getElementById('taskForm');
 const tableBody = document.querySelector('tbody');
 
-const tasks = [
-  {
-    id: 0,
-    name: 'Write pomodoro app',
-    pomodoroDone: 1,
-    pomodoroCount: 4,
-    isFinished: false,
-  },
-  {
-    id: 1,
-    name: 'Write pomodoro app',
-    pomodoroDone: 1,
-    pomodoroCount: 4,
-    isFinished: false,
-  },
-  {
-    id: 2,
-    name: 'Write pomodoro',
-    pomodoroDone: 1,
-    pomodoroCount: 4,
-    isFinished: true,
-  },
-];
+// function common events of each button
+function buttonEvent(button, func) {
+  Array.from(button).forEach((btn) => {
+    btn.addEventListener('click', func);
+  });
+}
 
 // func create tr element
-function createElement(taskName, pomodoroDone, pomodoroCount, isFinished, id) {
+function createElement(task) {
   const trElement = document.createElement('tr');
   trElement.classList.add('task');
-  trElement.setAttribute('id', id);
+  trElement.setAttribute('id', task.id);
 
   trElement.innerHTML = `
   <td class="task-details">
-    <span class="task-name">${taskName}</span>
+    <span class="task-name">${task.name}</span>
   </td>
   <td class="task-details">
-    <span class="pomodoro-done">${pomodoroDone}</span> /
-    <span class="pomodoro-count">${pomodoroCount}</span> pomodoro
+    <span class="pomodoro-done">${task.pomodoro}</span> /
+    <span class="pomodoro-count">${task.count}</span> pomodoro
   </td>
-  <td class="task-details">${isFinished ? `<span>Finished</span> <button type="button" class="btn btn-delete" data-id="${id}">Delete task</button>`
-    : `<button type="button" class="btn btn-done" data-id="${id}">Done</button>
-      <button type="button" class="btn btn-increase" data-id="${id}">Increase Pomodoro Count</button>
-      <button type="button" class="btn btn-delete" data-id="${id}">Delete task</button>`}</td>
+  <td class="task-details">${task.isFinished ? `<span>Finished</span> <button type="button" class="btn btn-delete" data-id="${task.id}">Delete task</button>`
+    : `<button type="button" class="btn btn-done" data-id="${task.id}">Done</button>
+      <button type="button" class="btn btn-increase" data-id="${task.id}">Increase Pomodoro Count</button>
+      <button type="button" class="btn btn-delete" data-id="${task.id}">Delete task</button>`}</td>
   `;
 
   return trElement;
 }
 
-// func find task from id
+// func find task element from id
 function findTask(indexTask) {
   const idTask = document.getElementById(indexTask);
   return idTask;
@@ -64,30 +50,36 @@ const deleteTask = (e) => {
   taskElement.remove();
 };
 
-// increase pomodoro
+// func increase, after click the number of pomodoro will increase
 const increaseTask = (e) => {
   const indexTask = e.target.dataset.id;
   const task = findTask(indexTask);
-  let pomodoroDoneValue = +task.childNodes[3].querySelector('.pomodoro-done').innerText;
-  const pomodoroCountValue = +task.childNodes[3].querySelector('.pomodoro-count').innerText;
+  const taskChild = task.childNodes[3];
+  const indexTaskInArray = tasks.findIndex((taskInArray) => taskInArray.id === +indexTask);
+
+  let pomodoroDoneValue = +taskChild.querySelector('.pomodoro-done').innerText;
+  const pomodoroCountValue = +taskChild.querySelector('.pomodoro-count').innerText;
 
   if (pomodoroDoneValue < pomodoroCountValue) {
-    tasks[indexTask].pomodoroDone += 1;
-    task.childNodes[3].querySelector('.pomodoro-done').innerHTML = `${pomodoroDoneValue += 1}`;
+    tasks[indexTaskInArray].pomodoroDone += 1;
+    taskChild.querySelector('.pomodoro-done').innerHTML = `${pomodoroDoneValue += 1}`;
   }
 };
 
-// done pomodoro
+// func done tasks, after click button Done and Increase will disable
 const doneTask = (e) => {
   const targetElement = e.target;
+  const targetElementParent = targetElement.parentElement;
 
   const indexTask = targetElement.dataset.id;
-  const btnDone = targetElement.parentElement.querySelector('.btn-done');
-  const btnIncrease = targetElement.parentElement.querySelector('.btn-increase');
-  const btnDelete = targetElement.parentElement.querySelector('.btn-delete');
+  const btnDone = targetElementParent.querySelector('.btn-done');
+  const btnIncrease = targetElementParent.querySelector('.btn-increase');
+  const btnDelete = targetElementParent.querySelector('.btn-delete');
   const spanElement = document.createElement('span');
 
-  tasks[indexTask].isFinished = true;
+  const indexTaskInArray = tasks.findIndex((taskInArray) => taskInArray.id === +indexTask);
+
+  tasks[indexTaskInArray].isFinished = true;
   spanElement.innerHTML = 'Finished ';
   targetElement.parentNode.insertBefore(spanElement, btnDelete);
 
@@ -95,37 +87,37 @@ const doneTask = (e) => {
   btnIncrease.remove();
 };
 
-// event of three buttons
+// func handle events in a task
 const handleEvents = () => {
   const buttonsDelete = document.getElementsByClassName('btn-delete');
   const buttonsDone = document.getElementsByClassName('btn-done');
   const buttonsIncrease = document.getElementsByClassName('btn-increase');
 
-  Array.from(buttonsDelete).forEach((btn) => {
-    btn.addEventListener('click', deleteTask);
-  });
-
-  Array.from(buttonsDone).forEach((btn) => {
-    btn.addEventListener('click', doneTask);
-  });
-
-  Array.from(buttonsIncrease).forEach((btn) => {
-    btn.addEventListener('click', increaseTask);
-  });
+  buttonEvent(buttonsDelete, deleteTask);
+  buttonEvent(buttonsDone, doneTask);
+  buttonEvent(buttonsIncrease, increaseTask);
 };
 
 // render table
 const renderTasks = () => {
-  tasks.forEach((task) => {
+  tasks.forEach((element) => {
+    const task = {
+      id: element.id,
+      name: element.name,
+      pomodoro: element.pomodoroDone,
+      count: element.pomodoroCount,
+      isFinished: element.isFinished,
+    };
+
     tableBody.append(
-      createElement(task.name, task.pomodoroDone, task.pomodoroCount, task.isFinished, task.id),
+      createElement(task),
     );
   });
 
   handleEvents();
 };
 
-// add tasks
+// add task
 const addTask = (e) => {
   // This event is used to avoid the page reload of the submit event
   e.preventDefault();
@@ -137,21 +129,30 @@ const addTask = (e) => {
   if (taskName === '') {
     errorMessage.style.display = 'block';
     inputTask.classList.add('valid');
-    errorMessage.innerText = 'Do not empty';
+    errorMessage.innerText = 'Task name is empty';
   } else {
+    const temp = {
+      name: taskName,
+      pomodoro: 0,
+      count: selectPomodoro,
+      isFinished: false,
+    };
+
+    const task = {
+      id: tasks.length - 1,
+      ...temp,
+    };
+
     tasks.push({
       id: tasks.length > 0 ? tasks.length : 0,
-      name: taskName,
-      pomodoroDone: 0,
-      pomodoroCount: selectPomodoro,
-      isFinished: false,
+      ...temp,
     });
 
     errorMessage.style.display = 'none';
     inputTask.classList.remove('valid');
 
     tableBody.appendChild(
-      createElement(taskName, 0, selectPomodoro, false, tasks.length - 1),
+      createElement(task),
     );
 
     handleEvents();
