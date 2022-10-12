9dcda1834f1f352c30832f41e50c9d54950a4194
LanHuong142002 <lanhuong142002@gmail.com>
2022-10-12 14:46:03 +0700

    #139 Refactor: import files into a file

--- javascript/big-practice/src/scripts/main.js
+++ javascript/big-practice/src/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('a');
+// eslint-disable-next-line import/extensions
+import NoteView from './views/noteView';
+import ListNoteView from './views/listNoteView';
+import ListNoteModel from './models/listNoteModel';
+import NoteController from './controllers/noteController';
+
+const listNoteModel = new ListNoteModel();
+
+const noteView = new NoteView();
+const listNoteView = new ListNoteView(noteView);
+
+const listNoteController = new NoteController(listNoteModel, listNoteView);
+listNoteController.init();
