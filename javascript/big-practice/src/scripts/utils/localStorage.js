e4754c2a67980e995277c41583d7080dcee04c67
LanHuong142002 <lanhuong142002@gmail.com>
2022-10-25 08:46:16 +0700

    #148 Refactor: add comments for localStorage and noteView

--- javascript/big-practice/src/scripts/utils/localStorage.js
+++ javascript/big-practice/src/scripts/utils/localStorage.js
@@ -1,10 +1,18 @@
 import STORAGE_KEYS from '../constants/storageKeys';
 
 export default class LocalStorage {
+  /**
+   * @description function set items to localStorage
+   * @param {Array} listNotes
+   */
   static setItems(listNotes) {
     localStorage.setItem(STORAGE_KEYS, JSON.stringify(listNotes));
   }
 
+  /**
+   * @description function get items from localStorage
+   * @returns {Array} listNotes
+   */
   static getItems() {
     return JSON.parse(localStorage.getItem(STORAGE_KEYS));
   }
