7e3ec3c8ad34f6abb1272255354bd2012906c455
LanHuong142002 <lanhuong142002@gmail.com>
2022-10-25 11:30:45 +0700

    #148 Refactor: add comments for localStorage function

--- javascript/big-practice/src/scripts/utils/localStorage.js
+++ javascript/big-practice/src/scripts/utils/localStorage.js
@@ -1,7 +1,9 @@
 export default class LocalStorage {
   /**
    * @description function set items to localStorage
-   * @param {Array}
+   *
+   * @param {String} key
+   * @param {Array} value
    */
   static setItems(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
@@ -9,7 +11,9 @@ export default class LocalStorage {
 
   /**
    * @description function get items from localStorage
-   * @returns {Array} listNotes
+   *
+   * @param {String} key
+   * @returns {Array}
    */
   static getItems(key) {
     return JSON.parse(localStorage.getItem(key));
