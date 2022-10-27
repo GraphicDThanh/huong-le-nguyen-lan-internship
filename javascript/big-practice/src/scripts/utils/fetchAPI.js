25578828551d258072a4c21b54fbd16b85ac001e
LanHuong142002 <lanhuong142002@gmail.com>
2022-10-27 17:29:07 +0700

    #154 Refactor: setup JSON server and add method, get/post/put/delete

---
+++ javascript/big-practice/src/scripts/utils/fetchAPI.js
@@ -0,0 +1,65 @@
+const URL = 'http://localhost:3000/notes';
+
+export const getData = async () => {
+  try {
+    const response = await fetch(URL);
+    const notes = await response.json();
+    return notes;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const postData = async (note) => {
+  try {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(note),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(URL, options);
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const deleteData = async (id) => {
+  try {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(`${URL}/${id}`, options);
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const putData = async (id, note) => {
+  try {
+    const options = {
+      method: 'PUT',
+      body: JSON.stringify(note),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(`${URL}/${id}`, options);
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
