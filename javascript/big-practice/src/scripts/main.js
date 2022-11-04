999930556d5f3dcd493e74a44ab171fac23d82e0
LanHuong142002 <lanhuong142002@gmail.com>
2022-11-04 14:28:34 +0700

    #160 Refactor: add function check login or index page

--- javascript/big-practice/src/scripts/main.js
+++ javascript/big-practice/src/scripts/main.js
@@ -1,10 +1,27 @@
 import ListNoteView from './views/listNoteView';
 import ListNoteModel from './models/listNoteModel';
 import NoteController from './controllers/noteController';
+import AuthenticationController from './controllers/authenticationController';
+import LoginModel from './models/loginModel';
+import LoginView from './views/loginView';
 
 const listNoteModel = new ListNoteModel();
 
 const listNoteView = new ListNoteView();
 
+const loginModel = new LoginModel();
+
+const loginView = new LoginView();
+
 const listNoteController = new NoteController(listNoteModel, listNoteView);
-listNoteController.init();
+const authenticationController = new AuthenticationController(loginModel, loginView);
+
+(() => {
+  const page = document.body.className;
+
+  if (page === 'loginPage') {
+    authenticationController.init();
+  } else {
+    listNoteController.init();
+  }
+})();
