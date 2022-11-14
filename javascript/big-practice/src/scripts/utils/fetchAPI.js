326c4786e58f1d5c4c4bab072a6a1a334d80dd9c
LanHuong142002 <lanhuong142002@gmail.com>
2022-11-14 14:11:27 +0700

    #171 Refactor: remove redundant method and rename method

--- javascript/big-practice/src/scripts/utils/fetchAPI.js
+++ javascript/big-practice/src/scripts/utils/fetchAPI.js
@@ -1,27 +1,6 @@
-import { API_MESSAGE } from '../constants/message';
 import URL_API from '../constants/apiUrl';
 
 const notesURL = `${URL_API.BASE_URL}${URL_API.NOTES_URL}`;
-const usersURL = `${URL_API.BASE_URL}${URL_API.USERS_URL}`;
-
-/**
- * @description function get list user or get user by key
- *
- * @param {String} email get from input login
- *
- * @returns {Object} user
- */
-export const getUser = async (key, param) => {
-  try {
-    const response = await fetch(key && param ? `${usersURL}?&${param}=${key}` : `${usersURL}`);
-    const users = await response.json();
-
-    return users;
-  } catch (error) {
-    const message = `${API_MESSAGE.GET} ${error}`;
-    throw message;
-  }
-};
 
 /**
  * @description function get note with id of note
@@ -41,15 +20,9 @@ export const getDataById = async (id) => {
   }
 };
 
-/**
- * @description function get data by user id
- * @param {String} id is user id
- *
- * @returns {Array} notes
- */
-export const getDataByUserId = async (id) => {
+export const getAllData = async () => {
   try {
-    const response = await fetch(`${notesURL}?&ownerId=${id}`);
+    const response = await fetch(`${notesURL}`);
     const notes = await response.json();
     return notes;
   } catch (e) {
