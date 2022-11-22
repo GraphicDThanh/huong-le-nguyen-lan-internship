8d4bf1472423a588cdab9e9db4ea909b82100c7a
LanHuong142002 <lanhuong142002@gmail.com>
2022-11-22 09:44:04 +0700

    #180 Refactor: add a common navigateHomePage

--- javascript/big-practice/src/scripts/helpers/eventHelpers.js
+++ javascript/big-practice/src/scripts/helpers/eventHelpers.js
@@ -1,3 +1,6 @@
+import STORAGE_KEYS from '../constants/storageKeys';
+import navigatePage from '../utils/navigatePage';
+
 export default class EventHelpers {
   /**
    * @description function stop overlay bubbling event of 2 input note form
@@ -5,6 +8,20 @@ export default class EventHelpers {
   stopEvents(element) {
     element.addEventListener('click', (e) => {
       e.stopPropagation();
+    });
+  }
+
+  /**
+   * @description function move to home page when
+   * click to element
+   *
+   * @param {Object} element is element want to
+   * add event click move to home page
+   */
+  navigateHomePage(element) {
+    element.addEventListener('click', () => {
+      navigatePage('home.html');
+      sessionStorage.setItem(STORAGE_KEYS.PAGE_NUMBER, '0');
     });
   }
 }
