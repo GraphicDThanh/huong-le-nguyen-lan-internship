448e377ce5722d3cd0fc38e0620746028076fa66
Huong <lanhuong142002@gmail.com>
2022-11-23 15:19:01 +0700

    #178 Refactor: add function clear input search

--- javascript/big-practice/src/scripts/controllers/noteController.js
+++ javascript/big-practice/src/scripts/controllers/noteController.js
@@ -57,6 +57,9 @@ export default class NoteController {
       // function render list notes
       this.view.renderListNotes(listNotes, handlers);
 
+      // function search note by value of input
+      this.view.bindSearchNotes((inputValue) => this.searchNote(inputValue));
+
       // function show Empty Note if note is empty
       this.view.showHideEmpty(listNotes, 'listNotes');
     } catch (error) {
@@ -162,5 +165,22 @@ export default class NoteController {
     } catch (error) {
       renderPopupError(error.message);
     }
+  }
+
+  /**
+   * @description function search note by value of input
+   * and if no note matches. It will show message
+   *
+   * @param {String} inputValue is value of input
+   */
+  searchNote(inputValue) {
+    const handlers = {
+      handleDeleteNote: (noteId) => this.deleteNote(noteId),
+      handleShowNoteForm: (id) => this.handleNoteForm(id),
+    };
+    const listNotes = this.model.searchNote(inputValue);
+
+    this.view.renderListNotes(listNotes, handlers);
+    this.view.searchNotFound(listNotes.length);
   }
 }
