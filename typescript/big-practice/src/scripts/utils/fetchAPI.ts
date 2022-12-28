296e7b5d2ffd96e80af27f25dab499f48a08df3c
Huong <lanhuong142002@gmail.com>
2022-12-28 14:54:16 +0700

    #213 Refactor: add type for utils, custom error based on status code

---
+++ typescript/big-practice/src/scripts/utils/fetchAPI.ts
@@ -0,0 +1,149 @@
+import URL_API from '../constants/apiUrl';
+import CustomError from './customError';
+import { statusError } from './handleError';
+
+export default class FetchAPI<T> {
+  baseURL: string;
+
+  constructor() {
+    this.baseURL = `${URL_API.BASE_URL}`;
+  }
+
+  /**
+   * @description function get all notes
+   *
+   * @param {String} url is endpoint
+   *
+   * @returns {Object} notes
+   */
+  async getAllItems(url: string): Promise<T[]> {
+    try {
+      const response = await fetch(`${this.baseURL}${url}`);
+      const items: T[] = await response.json();
+      const listItems = statusError(response, items);
+
+      return listItems;
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw Object.assign(error);
+      }
+
+      throw new Error('Fail to fetch');
+    }
+  }
+
+  /**
+   * @description function find notes by key
+   *
+   * @param {String} url is resource
+   * @param {String} key is endpoint of url
+   *
+   * @returns {Object} notes
+   */
+  async getItemByKey(url: string, key: string): Promise<T[]> {
+    try {
+      const response = await fetch(`${this.baseURL}${url}${key}`);
+      const items: T[] = await response.json();
+      const listItems = statusError(response, items);
+
+      return listItems;
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw Object.assign(error);
+      }
+
+      throw new Error('Fail to fetch');
+    }
+  }
+
+  /**
+   * @description function add new note to api
+   *
+   * @param {Object} note is a note
+   * @param {String} url is endpoint
+   *
+   * @return {Object} noteItem is returned after calling api
+   */
+  async postItem(note: T, url: string): Promise<T> {
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(note),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const response = await fetch(`${this.baseURL}${url}`, options);
+      const items: T = await response.json();
+      const item = statusError(response, items);
+
+      return item;
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw Object.assign(error);
+      }
+
+      throw new Error('Fail to fetch');
+    }
+  }
+
+  /**
+   * @description function delete note in api
+   *
+   * @param {String} id is id of note
+   * @param {String} url is endpoint
+   */
+  async deleteItem(id: string, url: string): Promise<T> {
+    try {
+      const options = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const response = await fetch(`${this.baseURL}${url}/${id}`, options);
+      const items: T = await response.json();
+      const item = statusError(response, items);
+
+      return item;
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw Object.assign(error);
+      }
+
+      throw new Error('Fail to fetch');
+    }
+  }
+
+  /**
+   * @description function update note with id
+   *
+   * @param {String} id is id of note
+   * @param {Object} note is note
+   * @param {String} url is endpoint
+   *
+   * @return {Object} noteItem is returned after calling api
+   */
+  async putItem(id: string, note: T, url: string): Promise<T> {
+    try {
+      const options = {
+        method: 'PATCH',
+        body: JSON.stringify(note),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const response = await fetch(`${this.baseURL}${url}/${id}`, options);
+      const items: T = await response.json();
+      const item = statusError(response, items);
+
+      return item;
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw Object.assign(error);
+      }
+
+      throw new Error('Fail to fetch');
+    }
+  }
+}
---
+++ typescript/big-practice/src/scripts/utils/handleError.ts
@@ -0,0 +1,119 @@
+import { POPUP_MESSAGE } from '../constants/message';
+import EventHelpers from '../helpers/eventHelpers';
+import renderConfirmPopup from './confirmPopup';
+import { selectDOMClass } from './querySelectDOM';
+import ElementHelpers from '../helpers/elementHelpers';
+import STATUS_CODE from '../constants/statusCode';
+import CodeError from './customError';
+
+const elementHelpers = new ElementHelpers();
+/**
+ * @description function hide error of field you want to hide by
+ * adding a class have properties hide and remove class valid
+ *
+ * @param {Object} element of input you want to hide message
+ * @param {Object} label is label of input
+ */
+const hideError = (element: HTMLElement, label: HTMLElement) => {
+  const error = element.parentElement?.querySelector<HTMLElement>(
+    '.message .message-error'
+  );
+  const errorIcon = element.parentElement?.querySelector<HTMLElement>(
+    '.message .error-icon'
+  );
+
+  if (error && errorIcon) {
+    error.innerText = '';
+    elementHelpers.addClass(errorIcon, 'hide');
+    elementHelpers.removeClass(label, 'error');
+    elementHelpers.removeClass(element, 'valid');
+  }
+};
+
+/**
+ * @description function show error of field you want to
+ * show and message error of this field by remove class
+ * hide and add class valid
+ *
+ * @param {Object} element of input you want to show message
+ * @param {String} message is message error of field
+ * @param {Object} label is label of input
+ */
+const showError = (
+  element: HTMLElement,
+  message: string,
+  label: HTMLElement
+) => {
+  const error = element.parentElement?.querySelector('.message .message-error');
+  const errorIcon = element.parentElement?.querySelector<HTMLElement>(
+    '.message .error-icon'
+  );
+
+  if (error && errorIcon) {
+    error.textContent = message;
+    elementHelpers.removeClass(errorIcon, 'hide');
+    elementHelpers.addClass(element, 'valid');
+    elementHelpers.addClass(label, 'error');
+  }
+};
+
+/**
+ * @description function render popup error message with message.
+ * And bind event to popup, if user click close. It will disappear
+ *
+ * @param {String} errorMessage is message error
+ */
+const renderPopupError = (errorMessage: string) => {
+  const overlayWrapper = selectDOMClass('.overlay-wrapper');
+  const eventHelpers = new EventHelpers();
+
+  if (overlayWrapper) {
+    const handler = () => {
+      overlayWrapper.innerHTML = '';
+    };
+    overlayWrapper.appendChild(
+      renderConfirmPopup(`${POPUP_MESSAGE.ERRORS_MSG}${errorMessage}`)
+    );
+
+    const btnClose = selectDOMClass('.btn-close-popup');
+    if (btnClose) {
+      eventHelpers.addEvent(btnClose, 'click', handler);
+    }
+  }
+};
+
+/**
+ * @description function custom error based on status code from
+ * response and item if this status is success
+ *
+ * @param {Object} response is response received after call api
+ * @param {Object} items is data received after call api
+ * @returns
+ */
+const statusError = <T>(response: Response, items: T) => {
+  switch (response.status) {
+    case STATUS_CODE.STATUS_200:
+      return items;
+    case STATUS_CODE.STATUS_400:
+      throw Object.assign(
+        new CodeError(`${response.status} Bad Request`, response.status)
+      );
+    case STATUS_CODE.STATUS_404:
+      throw Object.assign(
+        new CodeError(`${response.status} Page Not Found`, response.status)
+      );
+    case STATUS_CODE.STATUS_500:
+      throw Object.assign(
+        new CodeError(
+          `${response.status} Internal Server Error`,
+          response.status
+        )
+      );
+    default:
+      throw Object.assign(
+        new CodeError(`${response.status} Fail to fetch`, response.status)
+      );
+  }
+};
+
+export { hideError, showError, renderPopupError, statusError };
