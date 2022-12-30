d79dee7dde7e575828bd04751f8a1ac24ab0ade2
LanHuong142002 <lanhuong142002@gmail.com>
2022-12-30 17:12:36 +0700

    #215 Refactor: resolve conflict of 2 branch

--- typescript/big-practice/src/scripts/utils/handleError.ts
+++ typescript/big-practice/src/scripts/utils/handleError.ts
@@ -75,9 +75,67 @@ const renderPopupError = (errorMessage: string) => {
       renderConfirmPopup(`${POPUP_MESSAGE.ERRORS_MSG}${errorMessage}`)
     );
 
-  const btnClose = selectDOMClass('.btn-close-popup');
-  const eventHelpers = new EventHelpers();
-  eventHelpers.addEvent(btnClose, 'click', handler);
+    const btnClose = selectDOMClass('.btn-close-popup');
+    eventHelpers.addEvent(btnClose, 'click', handler);
+  }
+};
+
+/**
+ * @description function custom error based on status code from
+ * response and item if this status is success
+ *
+ * @param {Object} response is response received after call api
+ * @param {Object} items is data received after call api
+ * @returns
+ */
+const generateError = <T>(response: Response, items: T) => {
+  switch (response.status) {
+    case STATUS_CODE.OK:
+    case STATUS_CODE.CREATED:
+      return items;
+    case STATUS_CODE.BAD_REQUEST:
+      throw Object.assign(
+        new CustomError(`${response.status} Bad Request`, response.status)
+      );
+    case STATUS_CODE.UNAUTHORIZED:
+      throw Object.assign(
+        new CustomError(`${response.status} Unauthorized`, response.status)
+      );
+    case STATUS_CODE.FORBIDDEN:
+      throw Object.assign(
+        new CustomError(`${response.status} Forbidden`, response.status)
+      );
+    case STATUS_CODE.NOT_FOUND:
+      throw Object.assign(
+        new CustomError(`${response.status} Page Not Found`, response.status)
+      );
+    case STATUS_CODE.INTERNAL_SERVER_ERROR:
+      throw Object.assign(
+        new CustomError(
+          `${response.status} Internal Server Error`,
+          response.status
+        )
+      );
+    case STATUS_CODE.SERVER_UNAVAILABLE:
+      throw Object.assign(
+        new CustomError(
+          `${response.status} Service Unavailable`,
+          response.status
+        )
+      );
+    default:
+      throw Object.assign(
+        new CustomError(`${response.status} Fail to fetch`, response.status)
+      );
+  }
+};
+
+const checkCustomError = (error: unknown) => {
+  if (error instanceof CustomError) {
+    throw Object.assign(error);
+  } else {
+    throw new Error('Fail to fetch');
+  }
 };
 
 export {
