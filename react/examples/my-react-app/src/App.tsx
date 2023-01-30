0b76c2bf908f99387646aa42a91959ace7ea5d5f
LanHuong142002 <lanhuong142002@gmail.com>
2023-01-30 17:50:29 +0700

    #266 Refactor: add examples of introducing JSX, Rendering Elements, Components and Props

--- react/examples/my-react-app/src/App.tsx
+++ react/examples/my-react-app/src/App.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { FunctionAndClassComponents, Welcome } from './exercises/componentsAndProps';
+import {
+  EmbeddingExpressionJSX,
+  SpecifyingChildrenWithJSX,
+  RepresentsObjects,
+} from './exercises/introducingJSX';
+import Tick from './exercises/renderingElements';
+import { Comment } from './exercises/componentsAndProps';
 
 function App() {
+  const data = {
+    author: {
+      avatarUrl: 'https://i.stack.imgur.com/ONspp.png',
+      name: 'Sara',
+    },
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    date: new Date().toLocaleString(),
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
+      <EmbeddingExpressionJSX />
+      <SpecifyingChildrenWithJSX />
+      <RepresentsObjects />
+      <Tick />
+      <FunctionAndClassComponents name='abc' age={123} />
+      <Welcome name='Sara' />
+      <Welcome name='Cahal' />
+      <Welcome name='Edite' />
+      <Comment {...data} />
     </div>
   );
 }
