22f9f3299176b9f8338bedc30a7c7582f9cf42c7
LanHuong142002 <lanhuong142002@gmail.com>
2023-02-02 17:21:32 +0700

    #266 Refactor: add examples of Lifting State Up, Composition vs Inheritance

--- react/examples/my-react-app/src/App.tsx
+++ react/examples/my-react-app/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import Calculator from './exercises/liftingStateUp';
+import { TemperatureInput } from './exercises/liftingStateUp2';
+import { WelcomeDialog, SignUpDialog } from './exercises/compositionVSInheritance';
+
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <label htmlFor=''>Calculator</label>
+      <Calculator />
+      <br />
+      <TemperatureInput scale='c' />
+      <TemperatureInput scale='f' />
+      <br />
+      <WelcomeDialog />
+      <SignUpDialog />
     </div>
   );
 }
