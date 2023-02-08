2aeeef57111ce8214137c6a96b4bf36e91d6832b
LanHuong142002 <lanhuong142002@gmail.com>
2023-02-08 11:26:35 +0700

    #270 Refactor: add decorators for task component, change config custom.d.ts

--- react/examples/my-react-app/src/components/Task/Task.stories.tsx
+++ react/examples/my-react-app/src/components/Task/Task.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
+import { useState } from 'react';
 import Task from './Task';
 
 export default {
@@ -14,27 +15,44 @@ export default {
     onArchiveTask: { action: 'onArchiveTask' },
     onPinTask: { action: 'onPinTask' },
   },
+  decorators: [
+    (Story, context) => {
+      const { args } = context;
+      const [value, setValue] = useState('');
+
+      const handleChange = (e) => {
+        setValue(e.target.value);
+      };
+
+      return (
+        <div>
+          <p>{value}</p>
+          <Story {...args} title={value} handleOnChange={handleChange} />
+        </div>
+      );
+    },
+  ],
 } as ComponentMeta<typeof Task>;
 
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  title: 'Build date picker',
-  state: 'TASK_INBOX',
+  // title: 'Build date picker',
+  status: 'TASK_INBOX',
   id: '1',
 };
 
 export const Pinned = Template.bind({});
 Pinned.args = {
-  title: 'QA dropdown',
-  state: 'TASK_PINNED',
+  // title: 'QA dropdown',
+  status: 'TASK_PINNED',
   id: '2',
 };
 
 export const Archived = Template.bind({});
 Archived.args = {
-  title: 'Write schema for account menu',
-  state: 'TASK_ARCHIVED',
+  // title: 'Write schema for account menu',
+  status: 'TASK_ARCHIVED',
   id: '3',
 };
--- react/examples/my-react-app/src/components/Task/Task.tsx
+++ react/examples/my-react-app/src/components/Task/Task.tsx
@@ -5,35 +5,49 @@ import Star from '../../assets/star.svg';
 interface Task {
   title: string;
   id: string;
-  state: string;
+  status: string;
   onArchiveTask: () => void;
   onPinTask: () => void;
+  handleOnChange: () => void;
 }
 
-export default function Task({ title, id, state, onArchiveTask, onPinTask }: Task) {
+export default function Task({
+  title,
+  id,
+  status,
+  onArchiveTask,
+  onPinTask,
+  handleOnChange,
+}: Task) {
   return (
-    <div className={`list-item ${state}`}>
+    <div className={`list-item ${status}`}>
       <label htmlFor='checked' aria-label={`archiveTask-${id}`} className='checkbox'>
         <input
           type='checkbox'
           name='checked'
-          checked={state === 'TASK_ARCHIVED' && true}
+          checked={status === 'TASK_ARCHIVED' && true}
           id={`archiveTask-${id}`}
-          onChange={() => onArchiveTask()}
+          onChange={onArchiveTask}
         />
       </label>
       <label htmlFor='title' aria-label={title} className='title'>
-        <input type='text' value={title} readOnly={true} name='title' placeholder='Input title' />
+        <input
+          type='text'
+          value={title}
+          onChange={handleOnChange}
+          name='title'
+          placeholder='Input title'
+        />
       </label>
-      {state !== 'TASK_ARCHIVED' && (
+      {status !== 'TASK_ARCHIVED' && (
         <button
           className='pin-button'
-          onClick={() => onPinTask()}
+          onClick={onPinTask}
           id={`pinTask-${id}`}
           aria-label={`pinTask-${id}`}
           key={`pinTask-${id}`}
         >
-          <img src={Star as string} alt='' />
+          <img src={Star} alt='' />
         </button>
       )}
     </div>
