0180a3f311e224d585261cf648bfacb8c76a9202
LanHuong142002 <lanhuong142002@gmail.com>
2023-02-14 17:20:18 +0700

    #283 Refactor: create components button, typography and icon

--- react/practice-one/src/App.tsx
+++ react/practice-one/src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css';
+import { Button } from 'components/common/Button';
+import { Typography } from 'components/common/Typography';
+import { Icon } from 'components/common/Icon';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Button />
+      <Typography />
+      <Icon />
+    </div>
+  );
 }
 
 export default App;
---
+++ react/practice-one/src/components/common/Icon/index.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+import Search from 'assets/icons/search.svg';
+
+const Icon = () => {
+  return (
+    <figure className='icon icon-sm'>
+      <img src={Search} alt='' />
+    </figure>
+  );
+};
+
+export { Icon };
