bf2cd914406fe5dd9b444dc0635be7b4bd803cdf
LanHuong142002 <lanhuong142002@gmail.com>
2023-02-16 16:20:09 +0700

    #280 Refactor: add images, icons, remove redundant file css and import components

--- react/practice-one/src/App.tsx
+++ react/practice-one/src/App.tsx
@@ -1,20 +1,25 @@
 import './App.css';
 import { Button } from 'components/common/Button';
 import { Typography } from 'components/common/Typography';
-import { Icon } from 'components/common/Icon';
+import { Image } from 'components/common/Image';
 import { ListItem } from 'components/common/ListItem';
 import { List } from 'components/common/List';
 import { Input } from 'components/common/Input';
 import { Textarea } from 'components/common/Textarea';
 import { Select } from 'components/common/Select';
 import { TestimonialCard } from 'components/common/TestimonialCard';
+import { PricingCard } from 'components/common/PricingCard';
+import { ProjectCard } from 'components/common/ProjectCard';
+import { FeatureCard } from 'components/common/FeatureCard';
+import { Logo } from 'components/common/Logo';
+import logo from 'assets/icons/logo.svg';
 
 function App() {
   return (
     <div className='App'>
       <Button />
       <Typography />
-      <Icon />
+      <Image />
       <br />
       <ListItem />
       <br />
@@ -28,6 +33,14 @@ function App() {
       <Select />
       <br />
       <TestimonialCard />
+      <br />
+      <PricingCard />
+      <br />
+      <ProjectCard />
+      <br />
+      <FeatureCard />
+      <br />
+      <Logo logo={logo} href='#' />
     </div>
   );
 }
