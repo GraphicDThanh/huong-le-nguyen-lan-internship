33aed7d55eea0c0a9a3c983f6869b2898e61107b
LanHuong142002 <lanhuong142002@gmail.com>
2023-02-17 15:05:39 +0700

    #283 Refactor: add props, type for components button, image, list, list item, typography

--- react/practice-one/src/components/common/Image/index.tsx
+++ react/practice-one/src/components/common/Image/index.tsx
@@ -1,13 +1,22 @@
 import './index.css';
+import ImageProps from 'interface/image';
 
-const Image = (props) => {
+const Image = (props: ImageProps) => {
   return (
     <figure
       className={`image image-${props.size}`}
-      onClick={props.onClick}
+      onClick={(e) => props.onClick?.(e)}
+      onMouseLeave={(e) => props.onMouseLeave?.(e)}
+      onMouseEnter={(e) => props.onMouseEnter?.(e)}
       data-index={props.index}
     >
-      <img src={props.image} alt='' />
+      {props.href ? (
+        <a href={props.href}>
+          <img src={props.image} alt={props.alt} />
+        </a>
+      ) : (
+        <img src={props.image} alt={props.alt} />
+      )}
     </figure>
   );
 };
--- react/practice-one/src/components/common/ListItem/index.tsx
+++ react/practice-one/src/components/common/ListItem/index.tsx
@@ -1,15 +1,15 @@
 import { Image } from 'components/common/Image';
+import ListItemProps from 'interface/listItem';
 import './index.css';
 
-const ListItem = (props) => {
-  const TagName = props.tagName;
+const ListItem = (props: ListItemProps) => {
+  const { tagName = 'a' } = props;
+  const TagName = tagName;
 
   return (
     <li className='list-item'>
       {props.image && <Image image={props.image} size={props.size} />}
-      <TagName href='#' className='item'>
-        {props.title}
-      </TagName>
+      <TagName className='item'>{props.title}</TagName>
     </li>
   );
 };
