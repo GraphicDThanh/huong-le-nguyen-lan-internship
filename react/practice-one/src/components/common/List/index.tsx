c5f62cb5d2b4dd41b542a00f7b94ab69c87d1019
LanHuong142002 <lanhuong142002@gmail.com>
2023-02-22 16:45:15 +0700

    #283 Refactor: add background story for components, change some css and class of components, remove redundant css

--- react/practice-one/src/components/common/List/index.tsx
+++ react/practice-one/src/components/common/List/index.tsx
@@ -4,20 +4,20 @@ import './index.css';
 
 interface Props extends ListItemProps {
   listTitle?: string;
-  classes?: 'list-column' | 'list-row' | 'list-menu';
+  classesList?: 'list-column' | 'list-row' | 'list-menu' | 'list-link';
   listItem: ListItemProps[];
 }
 
 const List = (props: Props) => {
-  const { classes = 'list-column' } = props;
+  const { classesList = 'list-column' } = props;
 
   return (
-    <ul className={`list ${classes}`}>
+    <ul className={`list ${classesList}`}>
       {props.listTitle && <p className='title'>{props.listTitle}</p>}
       {props.listItem.map((item, index) => (
         <ListItem
           image={item.image}
-          size={props.size}
+          size={item.size}
           title={item.title}
           tagName={props.tagName}
           key={index}
