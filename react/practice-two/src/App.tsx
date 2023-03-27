cd0739da6189c9340292c87e560e9d7b4353b278
LanHuong142002 <lanhuong142002@gmail.com>
2023-03-27 11:30:11 +0700

    #311 Refactor: create Home Layout and move code from Home Page to Home Layout, remove redundant CSS code

--- react/practice-two/src/App.tsx
+++ react/practice-two/src/App.tsx
@@ -1,22 +1,13 @@
 // Styles
 import './styles/main.css';
 
-// Layouts
-import { Header } from '@layouts';
-
 // Pages
 import { HomePage } from '@pages';
-
-// Contexts
-import { ModalProvider } from '@contexts';
 
 const App = () => {
   return (
     <div className='container'>
-      <Header />
-      <ModalProvider>
-        <HomePage />
-      </ModalProvider>
+      <HomePage />
     </div>
   );
 };
